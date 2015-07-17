a1828d3bf8dee17b1721f0ca46f8a350c88d81e9
matieli <ma_tl@suixingpay.com>
2015-07-17 11:44:42 +0800

    V1 branch add v1.java

---
+++ src/main/java/com/lemon/amq/consumer/V1.java
@@ -0,0 +1,19 @@
+/**
+ * Description: TODO
+ * Copyright:   ©2015 Vbill Payment Co. Ltd. All rights reserved.
+ * Created on:  2015年7月17日 上午11:40:05 
+ * @author ma_tl@suixingpay.com
+ */
+package com.lemon.amq.consumer;
+
+/**
+ * Description: TODO Copyright: ©2015 Vbill Payment Co. Ltd. All rights
+ * reserved. Created on: 2015年7月17日 上午11:40:05
+ * 
+ * @author ma_tl@suixingpay.com
+ */
+public class V1 {
+	public static void main(String[] args) {
+		System.out.println("测试V1");
+	}
+}
