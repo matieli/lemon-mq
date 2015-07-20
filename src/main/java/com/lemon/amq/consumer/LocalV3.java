40357b2a6baf9ae96d4d4e1b3537716e562c5e69
matieli <ma_tl@suixingpay.com>
2015-07-20 14:26:03 +0800

    创建localV3

---
+++ src/main/java/com/lemon/amq/consumer/LocalV3.java
@@ -0,0 +1,17 @@
+/**
+ * Description: TODO
+ * Copyright:   ©2015 Vbill Payment Co. Ltd. All rights reserved.
+ * Created on:  2015年7月20日 下午2:25:44 
+ * @author ma_tl@suixingpay.com
+ */
+package com.lemon.amq.consumer;
+
+/**
+ * Description: TODO
+ * Copyright:   ©2015 Vbill Payment Co. Ltd. All rights reserved.
+ * Created on:  2015年7月20日 下午2:25:44 
+ * @author ma_tl@suixingpay.com
+ */
+public class LocalV3 {
+
+}
